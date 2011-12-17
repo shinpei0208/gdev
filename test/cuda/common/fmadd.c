bf9c063258ca20541fd77fc720ccf3f647ae597a
Shinpei Kato <shinpei@cs.ucsc.edu>
2011-12-16 16:08:55 -0800

    test: added CUDA fmadd test

---
+++ test/cuda/common/fmadd.c
@@ -0,0 +1,266 @@
+#include <cuda.h>
+#ifdef __KERNEL__ /* just for measurement */
+#include <linux/vmalloc.h>
+#include <linux/time.h>
+#define printf printk
+#define malloc vmalloc
+#define free vfree
+#define gettimeofday(x, y) do_gettimeofday(x)
+#else /* just for measurement */
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#endif
+
+/* tvsub: ret = x - y. */
+static inline void tvsub(struct timeval *x, 
+						 struct timeval *y, 
+						 struct timeval *ret)
+{
+	ret->tv_sec = x->tv_sec - y->tv_sec;
+	ret->tv_usec = x->tv_usec - y->tv_usec;
+	if (ret->tv_usec < 0) {
+		ret->tv_sec--;
+		ret->tv_usec += 1000000;
+	}
+}
+
+int cuda_test_madd(unsigned int n, char *path)
+{
+	int i, j, idx;
+	CUresult res;
+	CUdevice dev;
+	CUcontext ctx;
+	CUfunction function;
+	CUmodule module;
+	CUdeviceptr a_dev, b_dev, c_dev;
+	float *a = (float *) malloc (n*n * sizeof(float));
+	float *b = (float *) malloc (n*n * sizeof(float));
+	float *c = (float *) malloc (n*n * sizeof(float));
+	int block_x, block_y, grid_x, grid_y;
+	int offset;
+	char fname[256];
+	struct timeval tv;
+	struct timeval tv_total_start, tv_total_end;
+	unsigned long total;
+	struct timeval tv_h2d_start, tv_h2d_end;
+	unsigned long h2d;
+	struct timeval tv_d2h_start, tv_d2h_end;
+	unsigned long d2h;
+	struct timeval tv_exec_start, tv_exec_end;
+	unsigned long exec;
+
+	/* initialize A[] & B[] */
+	for (i = 0; i < n; i++) {
+		for(j = 0; j < n; j++) {
+			idx = i * n + j;
+			a[idx] = i;
+			b[idx] = i;
+		}
+	}
+
+	/* block_x * block_y should not exceed 512. */
+	block_x = n < 16 ? n : 16;
+	block_y = n < 16 ? n : 16;
+	grid_x = n / block_x;
+	if (n % block_x != 0)
+		grid_x++;
+	grid_y = n / block_y;
+	if (n % block_y != 0)
+		grid_y++;
+	printf("block = (%d, %d)\n", block_x, block_y);
+	printf("grid = (%d, %d)\n", grid_x, grid_y);
+
+	gettimeofday(&tv_total_start, NULL);
+
+	res = cuInit(0);
+	if (res != CUDA_SUCCESS) {
+		printf("cuInit failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+
+	res = cuDeviceGet(&dev, 0);
+	if (res != CUDA_SUCCESS) {
+		printf("cuDeviceGet failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+
+	res = cuCtxCreate(&ctx, 0, dev);
+	if (res != CUDA_SUCCESS) {
+		printf("cuCtxCreate failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+
+	sprintf(fname, "%s/fmadd_gpu.cubin", path);
+	res = cuModuleLoad(&module, fname);
+	if (res != CUDA_SUCCESS) {
+		printf("cuModuleLoad() failed\n");
+		return -1;
+	}
+	res = cuModuleGetFunction(&function, module, "_Z3addPfS_S_f");
+	if (res != CUDA_SUCCESS) {
+		printf("cuModuleGetFunction() failed\n");
+		return -1;
+	}
+	res = cuFuncSetSharedSize(function, 0x40); /* just random */
+	if (res != CUDA_SUCCESS) {
+		printf("cuFuncSetSharedSize() failed\n");
+		return -1;
+	}
+	res = cuFuncSetBlockShape(function, block_x, block_y, 1);
+	if (res != CUDA_SUCCESS) {
+		printf("cuFuncSetBlockShape() failed\n");
+		return -1;
+	}
+
+	/* a[] */
+	res = cuMemAlloc(&a_dev, n*n * sizeof(float));
+	if (res != CUDA_SUCCESS) {
+		printf("cuMemAlloc (a) failed\n");
+		return -1;
+	}
+	/* b[] */
+	res = cuMemAlloc(&b_dev, n*n * sizeof(float));
+	if (res != CUDA_SUCCESS) {
+		printf("cuMemAlloc (b) failed\n");
+		return -1;
+	}
+	/* c[] */
+	res = cuMemAlloc(&c_dev, n*n * sizeof(float));
+	if (res != CUDA_SUCCESS) {
+		printf("cuMemAlloc (c) failed\n");
+		return -1;
+	}
+
+	gettimeofday(&tv_h2d_start, NULL);
+	/* upload a[] and b[] */
+	res = cuMemcpyHtoD(a_dev, a, n*n * sizeof(float));
+	if (res != CUDA_SUCCESS) {
+		printf("cuMemcpyHtoD (a) failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+	res = cuMemcpyHtoD(b_dev, b, n*n * sizeof(float));
+	if (res != CUDA_SUCCESS) {
+		printf("cuMemcpyHtoD (b) failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+	gettimeofday(&tv_h2d_end, NULL);
+
+	/* set kernel parameters */
+	offset = 0;
+	res = cuParamSetv(function, offset, &a_dev, sizeof(a_dev));	
+	if (res != CUDA_SUCCESS) {
+		printf("cuParamSeti (a) failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+	offset += sizeof(a_dev);
+	res = cuParamSetv(function, offset, &b_dev, sizeof(b_dev));
+	if (res != CUDA_SUCCESS) {
+		printf("cuParamSeti (b) failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+	offset += sizeof(b_dev);
+	res = cuParamSetv(function, offset, &c_dev, sizeof(c_dev));
+	if (res != CUDA_SUCCESS) {
+		printf("cuParamSeti (c) failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+	offset += sizeof(c_dev);
+	res = cuParamSetv(function, offset, &n, sizeof(n));
+	if (res != CUDA_SUCCESS) {
+		printf("cuParamSeti (c) failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+	offset += sizeof(n);
+	res = cuParamSetSize(function, offset);
+	if (res != CUDA_SUCCESS) {
+		printf("cuParamSetSize failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+
+	gettimeofday(&tv_exec_start, NULL);
+	/* launch the kernel */
+	res = cuLaunchGrid(function, grid_x, grid_y);
+	if (res != CUDA_SUCCESS) {
+		printf("cuLaunchGrid failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+	cuCtxSynchronize();
+	gettimeofday(&tv_exec_end, NULL);
+
+	gettimeofday(&tv_d2h_start, NULL);
+	/* download c[] */
+	res = cuMemcpyDtoH(c, c_dev, n*n * sizeof(float));
+	if (res != CUDA_SUCCESS) {
+		printf("cuMemcpyDtoH (c) failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+	gettimeofday(&tv_d2h_end, NULL);
+
+	res = cuMemFree(a_dev);
+	if (res != CUDA_SUCCESS) {
+		printf("cuMemFree (a) failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+	res = cuMemFree(b_dev);
+	if (res != CUDA_SUCCESS) {
+		printf("cuMemFree (b) failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+	res = cuMemFree(c_dev);
+	if (res != CUDA_SUCCESS) {
+		printf("cuMemFree (c) failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+
+	res = cuModuleUnload(module);
+	if (res != CUDA_SUCCESS) {
+		printf("cuModuleUnload failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+
+	res = cuCtxDestroy(ctx);
+	if (res != CUDA_SUCCESS) {
+		printf("cuCtxDestroy failed: res = %lu\n", (unsigned long)res);
+		return -1;
+	}
+
+	gettimeofday(&tv_total_end, NULL);
+
+	/* check the results */
+	i = j = idx = 0;
+	while (i < n) {
+		while (j < n) {
+			idx = i * n + j;
+			if (c[idx] != a[idx] + b[idx]) {
+				printf("c[%d] = %d\n", idx, c[idx]);
+				printf("a[%d]+b[%d] = %d\n", idx, idx, a[idx]+b[idx]);
+				return -1;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	free(a);
+	free(b);
+	free(c);
+
+	tvsub(&tv_h2d_end, &tv_h2d_start, &tv);
+	h2d = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	tvsub(&tv_d2h_end, &tv_d2h_start, &tv);
+	d2h = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	tvsub(&tv_exec_end, &tv_exec_start, &tv);
+	exec = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	tvsub(&tv_total_end, &tv_total_start, &tv);
+	total = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+
+	printf("HtoD: %lu\n", h2d);
+	printf("DtoH: %lu\n", d2h);
+	printf("Exec: %lu\n", exec);
+	printf("Total: %lu\n", total);
+
+	return 0;
+}
+
+
