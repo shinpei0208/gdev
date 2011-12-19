043ce5ca7fd5fce4b3d9a8672a956716d049c467
Shinpei Kato <shinpei@cs.ucsc.edu>
2011-12-19 11:48:35 -0800

    test: added CUDA fmmul test

---
+++ test/cuda/user/fmmul/fmmul_gpu.cu
@@ -0,0 +1,1 @@
+../../common/fmmul_gpu.cu
---
+++ test/cuda/user/fmmul/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int cuda_test_fmmul(unsigned int n, char *path);
+
+int main(int argc, char *argv[])
+{
+	unsigned int n = 3;
+
+	if (argc > 1)
+		n = atoi(argv[1]);
+
+	if (cuda_test_fmmul(n, ".") < 0)
+		printf("Test failed\n");
+	else
+		printf("Test passed\n");
+	
+	return 0;
+}
