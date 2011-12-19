de86d8757d24051687442fdbfb14e40ef665650a
Shinpei Kato <shinpei@cs.ucsc.edu>
2011-12-19 11:48:35 -0800

    test: added CUDA fmmul test

---
+++ test/cuda/common/fmmul_gpu.cu
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <cuda.h>
+__global__
+void mul(float *a, float *b, float *c, int n)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < n && j < n) {
+        int idx = i * n + j;
+        c[idx] = a[idx] * b[idx];
+    }
+}
